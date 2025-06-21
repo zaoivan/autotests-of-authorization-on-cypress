205a88dbee95958a4fcd7089cf97f1315db45ad5
Yelena Zaostrovskikh <zaoivan@gmail.com>
2025-06-21 12:23:01 +0500

    добавил after each

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -5,13 +5,16 @@ describe('Проверка авторизации', function () {
          cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
            });
 
+   afterEach('Конец теста', function () {
+         cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+        });
+
    it('Правильный логин и правильный пароль', function () { // позитивный кейс
         cy.get('#mail').type('german@dolnikov.ru');
         cy.get('#pass').type('iLoveqastudio1');
         cy.get('#loginButton').click();
         cy.get('#messageHeader').should('be.visible');
         cy.get('#messageHeader').contains('Авторизация прошла успешно');
-        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
 
     })
        it('Восстановление пароля', function () { // позитивный кейс
@@ -19,7 +22,6 @@ describe('Проверка авторизации', function () {
         cy.get('#mailForgot').type('german@dolnikov.ru');
         cy.get('#restoreEmailButton').click();
         cy.get('#messageHeader').contains('Успешно отправили пароль на e-mail');
-        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
     })
 
       it('Правильный логин и НЕправильный пароль', function () { // негативный кейс
@@ -28,7 +30,6 @@ describe('Проверка авторизации', function () {
         cy.get('#loginButton').click();
         cy.get('#messageHeader').should('be.visible');
         cy.get('#messageHeader').contains('Такого логина или пароля нет');
-        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
     })
       it('НЕправильный логин и правильный пароль', function () { // негативный кейс
         cy.get('#mail').type('ivan@dolnikov.ru'); // НЕправильный логин
@@ -36,7 +37,6 @@ describe('Проверка авторизации', function () {
         cy.get('#loginButton').click();
         cy.get('#messageHeader').should('be.visible');
         cy.get('#messageHeader').contains('Такого логина или пароля нет');
-        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
     })
 
       it('Валидация на наличие @', function () { // негативный кейс
@@ -45,7 +45,6 @@ describe('Проверка авторизации', function () {
         cy.get('#loginButton').click();
         cy.get('#messageHeader').should('be.visible');
         cy.get('#messageHeader').contains('Нужно исправить проблему валидации');
-        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
     })
 
     it('Проверка на приведение к строчным буквам в логине', function () {
@@ -54,13 +53,16 @@ describe('Проверка авторизации', function () {
         cy.get('#loginButton').click();
         cy.get('#messageHeader').should('be.visible');
         cy.get('#messageHeader').contains('Авторизация прошла успешно');
-        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
         //Разработчик допустил баг в этом месте и не реализовал пункт #2 из требований.
         //Тест должен упасть — и это ок (то есть мы этим тестом поймали баг, который допустил разработчик
     })
  })
 
 // запуск через теринал: npx cypress run --spec cypress/e2e/login.cy.js --browser chrome
+
+// npx cypress open // запустить cypress
+// npx cypress run // запустить прогон автотестов из терминала
+
 // не мог запустить команду выше, терминал писал "Need to install ...", требовал обновить Cypress до последней версии v14...,
 // загуглил, gemini посоветовал в package.json было "cypress": "^12.7.0" - но это не помогло
 // помог copilot
@@ -72,5 +74,3 @@ describe('Проверка авторизации', function () {
 // git add . // проиндексировать все файлы
 // git commit -m "имя_коммита" // добавить комментарий для фиксации
 // git push // загрузить изменения с ноута в github
-
-
